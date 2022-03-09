f3426492d67ebf6d508e7c945b4347f50ac1a1a1
Kyle Cunningham <kyle@codeincarnate.com>
2022-03-09 15:39:33 -0600

    Set button active state

--- src/components/CameraControls.tsx
+++ src/components/CameraControls.tsx
@@ -8,6 +8,8 @@ interface Props {
 }
 
 export const CameraControls: React.FC<Props> = ({cameraOpts, updateCameraOpts}) => {
+  const { type, viewPlane } = cameraOpts;
+
   const style: any = {
     position: 'absolute',
     zIndex: 1,
@@ -15,10 +17,26 @@ export const CameraControls: React.FC<Props> = ({cameraOpts, updateCameraOpts}) 
       
   return (
     <ToolbarButtonRow style={style}>
-      <ToolbarButton onClick={() => updateCameraOpts({type: "perspective"})}>3D</ToolbarButton>
-      <ToolbarButton onClick={() => updateCameraOpts({type: "orthographic", viewPlane: "x"})}>X</ToolbarButton>
-      <ToolbarButton onClick={() => updateCameraOpts({type: "orthographic", viewPlane: "y"})}>Y</ToolbarButton>
-      <ToolbarButton onClick={() => updateCameraOpts({type: "orthographic", viewPlane: "z"})}>Z</ToolbarButton>
+      <ToolbarButton 
+        variant={type == 'perspective' ? 'active' : 'default'} 
+        onClick={() => updateCameraOpts({type: "perspective"})}>
+          3D
+      </ToolbarButton>
+      <ToolbarButton 
+        variant={type == 'orthographic' && viewPlane == 'x' ? 'active' : 'default'}
+        onClick={() => updateCameraOpts({type: "orthographic", viewPlane: "x"})}>
+          X
+      </ToolbarButton>
+      <ToolbarButton
+        variant={type == 'orthographic' && viewPlane == 'y' ? 'active' : 'default'}
+        onClick={() => updateCameraOpts({type: "orthographic", viewPlane: "y"})}>
+        Y
+        </ToolbarButton>
+      <ToolbarButton 
+        variant={type == 'orthographic' && viewPlane == 'z' ? 'active' : 'default'}
+        onClick={() => updateCameraOpts({type: "orthographic", viewPlane: "z"})}>
+          Z
+      </ToolbarButton>
     </ToolbarButtonRow>
   );
 };
