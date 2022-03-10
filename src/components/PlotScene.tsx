6d18cdd8459f354774e93f86979f8bb9d5517063
Kyle Cunningham <kyle@codeincarnate.com>
2022-03-09 21:58:04 -0600

    Add selective bloom effects

--- src/components/PlotScene.tsx
+++ src/components/PlotScene.tsx
@@ -1,5 +1,5 @@
 import { DataFrame } from '@grafana/data';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, RefObject, ReactNode } from 'react';
 import { Direction, ScatterPlotOptions } from 'types';
 import { getIntervalLabels, prepData } from 'utils';
 import { Grid } from './Grid';
@@ -8,9 +8,10 @@ import OptionsContext from 'optionsContext';
 
 interface Props {
   frames: DataFrame[];
+  lights: RefObject<ReactNode>[];
 }
 
-export const PlotScene: React.FC<Props> = ({ frames }) => {
+export const PlotScene: React.FC<Props> = ({ frames, lights }) => {
   const options: ScatterPlotOptions = useContext(OptionsContext);
 
   const size = options.sceneScale;
@@ -40,7 +41,7 @@ export const PlotScene: React.FC<Props> = ({ frames }) => {
 
   return (
     <>
-      <PointCloud currentPoints={pointData} oldPoints={oldPointData}/>
+      <PointCloud currentPoints={pointData} oldPoints={oldPointData} lights={lights} />
       <group>
         <Grid
           direction={Direction.Up}
--- src/components/PointCloud.tsx
+++ src/components/PointCloud.tsx
@@ -6,25 +6,30 @@
 
 import { hexToRgb } from '../utils';
 import OptionsContext from 'optionsContext';
-import React, { useRef, useContext, useEffect, useState } from 'react';
+import React, { useRef, useContext, useEffect, useState, RefObject, ReactNode } from 'react';
 import { PointData, RGBColor } from 'types';
 import { PointsMaterial } from 'three';
 import { useFrame } from '@react-three/fiber';
 import { lerp } from 'three/src/math/MathUtils';
+import { EffectComposer, SelectiveBloom } from '@react-three/postprocessing';
 
 interface Props {
   currentPoints: PointData;
   oldPoints: PointData;
+  lights: RefObject<ReactNode>[];
   onPointerOver?: Function;
   onPointerOut?: Function;
 }
 
-export const PointCloud: React.FC<Props> = ({ currentPoints, oldPoints }) => {
+export const PointCloud: React.FC<Props> = ({ currentPoints, oldPoints, lights}) => {
   const colorAttrRef: any = useRef(null);
+  const pointsRef = useRef(null);
   const materialRef = useRef({} as PointsMaterial);
   const dataPointColor: string = useContext(OptionsContext).dataPointColor;
   const [points, setPoints] = useState(null as any);
   let showPoints = true;
+
+  console.log(lights);
 
   useEffect(() => {
     if (colorAttrRef.current) {
@@ -57,34 +62,55 @@ export const PointCloud: React.FC<Props> = ({ currentPoints, oldPoints }) => {
     (state, delta) => (materialRef.current.opacity = lerp(materialRef.current.opacity, showPoints ? 1 : 0, 0.1))
   );
 
+  let bloom = null;
+  if (pointsRef.current !== null && lights[0].current !== null) {
+    bloom = (
+      <EffectComposer>
+        <SelectiveBloom
+          lights={lights}
+          selection={pointsRef}
+          kernelSize={2} 
+          luminanceThreshold={0} 
+          luminanceSmoothing={0.4} 
+          intensity={1}
+        />
+      </EffectComposer>
+    )
+  }
+
+  console.log(bloom);
+
   return (
     <>
-    {points && <points>
-      <bufferGeometry attach="geometry">
-        <bufferAttribute
-          attachObject={['attributes', 'position']}
-          count={points.points.length / 3}
-          array={points.points}
-          itemSize={3}
-        />
-        <bufferAttribute
-          ref={colorAttrRef}
-          attachObject={['attributes', 'color']}
-          count={points.colors.length / 3}
-          array={points.colors}
-          itemSize={3}
-        />
-      </bufferGeometry>
-      <pointsMaterial
-        ref={materialRef}
-        attach="material"
-        opacity={0}
-        transparent
-        vertexColors
-        size={0.9}
-        sizeAttenuation={true}
-      />
-    </points>}
+      {points && (
+          <points ref={pointsRef}>
+            <bufferGeometry attach="geometry">
+              <bufferAttribute
+                attachObject={['attributes', 'position']}
+                count={points.points.length / 3}
+                array={points.points}
+                itemSize={3}
+              />
+              <bufferAttribute
+                ref={colorAttrRef}
+                attachObject={['attributes', 'color']}
+                count={points.colors.length / 3}
+                array={points.colors}
+                itemSize={3}
+              />
+            </bufferGeometry>
+            <pointsMaterial
+              ref={materialRef}
+              attach="material"
+              opacity={0}
+              transparent
+              vertexColors
+              size={0.9}
+              sizeAttenuation={true}
+            />
+          </points>
+      )}
+      {bloom}
     </>
   );
 };
