da17642d9037f4f98c5ce25b48f4adda21c29555
Kyle Cunningham <kyle@codeincarnate.com>
2022-10-24 13:33:50 +0700

    Fix linter errors

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,30 +1,31 @@
 import React, { useContext } from 'react';
-import { Text }  from '@react-three/drei';
+import { Text } from '@react-three/drei';
 import { Direction, LabelProps } from 'types';
 import { SCENE_SCALE, WHITE } from 'consts';
 import OptionsContext from 'optionsContext';
 import { ScatterPlotOptions } from 'models.gen';
 
-export const Label = ({direction, labelSize, position, rotation, text}: LabelProps) => {
+export const Label = ({ direction, labelSize, position, rotation, text }: LabelProps) => {
   const options: ScatterPlotOptions = useContext(OptionsContext);
   const fontSize = labelSize ? labelSize : (SCENE_SCALE * 0.2) / 10;
   const color = options.themeColor ?? WHITE;
 
   // Negative percentages don't work beyond -100% hence using
   // local units in that case instead
-  const anchorX = direction === Direction.Right ? "115%" : -1.15;
+  const anchorX = direction === Direction.Right ? '115%' : -1.15;
 
   return (
-    <Text 
-      color={ color } 
+    <Text
+      color={color}
       font="/public/plugins/grafana-labs-grafana-3-d-scatter-panel/fonts/Roboto-Regular.ttf"
       textAlign="left"
       //@ts-ignore
-      anchorX={ anchorX }
-      fontSize={ fontSize }
-      position={ position }
-      rotation={ rotation }> 
-      { text }  
+      anchorX={anchorX}
+      fontSize={fontSize}
+      position={position}
+      rotation={rotation}
+    >
+      {text}
     </Text>
   );
 };
