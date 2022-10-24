cf78671a60584746affdcc9542b2f62b0ea27d4d
Kyle Cunningham <kyle@codeincarnate.com>
2022-10-24 13:22:28 +0700

    Using local unit anchoring to position text.

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Text }  from '@react-three/drei';
-import { LabelProps } from 'types';
+import { Direction, LabelProps } from 'types';
 import { SCENE_SCALE, WHITE } from 'consts';
 import OptionsContext from 'optionsContext';
 import { ScatterPlotOptions } from 'models.gen';
@@ -10,11 +10,17 @@ export const Label = ({direction, labelSize, position, rotation, text}: LabelPro
   const fontSize = labelSize ? labelSize : (SCENE_SCALE * 0.2) / 10;
   const color = options.themeColor ?? WHITE;
 
+  // Negative percentages don't work beyond -100% hence using
+  // local units in that case instead
+  const anchorX = direction === Direction.Right ? "115%" : -1.15;
+
   return (
     <Text 
       color={ color } 
       font="/public/plugins/grafana-labs-grafana-3-d-scatter-panel/fonts/Roboto-Regular.ttf"
       textAlign="left"
+      //@ts-ignore
+      anchorX={ anchorX }
       fontSize={ fontSize }
       position={ position }
       rotation={ rotation }> 
